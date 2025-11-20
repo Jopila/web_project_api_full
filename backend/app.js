7f2e29c82972f94679517ee4460e5ea701e05a36
Jopila <marcelomarangoni767@gmail.com>
2025-11-20 12:20:19 -0300

    conexao do frontend com o backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const { createUser, login } = require('./controllers/users');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const auth = require('./middlewares/auth');
-const errorHandler = require('./middlewares/errorHandler');
-const { validateSignup, validateSignin } = require('./middlewares/validations');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const express = require("express");
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+const { createUser, login } = require("./controllers/users");
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
+const auth = require("./middlewares/auth");
+const errorHandler = require("./middlewares/errorHandler");
+const { validateSignup, validateSignin } = require("./middlewares/validations");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
@@ -15,16 +16,16 @@ const PORT = 3000;
 app.use(express.json());
 app.use(requestLogger);
 
-app.post('/signin', validateSignin, login);
-app.post('/signup', validateSignup, createUser);
+app.post("/signin", validateSignin, login);
+app.post("/signup", validateSignup, createUser);
 
 app.use(auth);
 
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.use("/users", usersRouter);
+app.use("/cards", cardsRouter);
 
 app.use((req, res) => {
-  const error = new Error('A solicitacao nao foi encontrada');
+  const error = new Error("A solicitacao nao foi encontrada");
   error.statusCode = 404;
   throw error;
 });
@@ -34,12 +35,14 @@ app.use(errors());
 app.use(errorHandler);
 
 mongoose
-  .connect('mongodb://localhost:27017/aroundb')
+  .connect(
+    `mongodb+srv://marcelomarangoni767_db_user:${process.env.DB_PASSWORD}@cluster0.prse0q9.mongodb.net/aroundb`
+  )
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Servidor ouvindo na porta ${PORT}`);
     });
   })
   .catch((error) => {
-    console.error('Erro ao conectar ao MongoDB', error);
+    console.error("Erro ao conectar ao MongoDB", error);
   });
